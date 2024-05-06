658a394e21d56654753beb3cd21b354397603a89
Sylkadi <klagu21@gmail.com>
2024-05-06 22:04:52 +0100

    -0.13.6 -Removed removal of masked scrap from noMasks event, though I will need to reImplement this feature back later. This caused crashes.

--- BrutalCompanyMinus/Minus/Events/NoMasks.cs
+++ BrutalCompanyMinus/Minus/Events/NoMasks.cs
@@ -5,15 +5,11 @@ using System.Text;
 using System.Threading.Tasks;
 using Unity.Netcode;
 using UnityEngine;
-using static MonoMod.Cil.RuntimeILReferenceBag.FastDelegateInvokers;
 
 namespace BrutalCompanyMinus.Minus.Events
 {
     internal class NoMasks : MEvent
     {
-        public static bool removedComedy, removedTragedy;
-        public static SpawnableItemWithRarity comedyReference, tragedyRefernece;
-
         public override string Name() => nameof(NoMasks);
 
         public static NoMasks Instance;
@@ -25,58 +21,13 @@ namespace BrutalCompanyMinus.Minus.Events
             EventsToRemove = new List<string>() { nameof(Hell), nameof(Masked), nameof(NutSlayer) };
 
             Weight = 1;
-            Descriptions = new List<string>() { "No masks", "No friends :(", "No more hugs", "No more trust issues" };
+            Descriptions = new List<string>() { "No friends :(", "No more hugs", "No more trust issues" };
             ColorHex = "#008000";
             Type = EventType.Remove;
         }
 
-        public override bool AddEventIfOnly()
-        {
-            if (Manager.SpawnExists(Assets.EnemyName.Masked)) return true;
-            if (RoundManager.Instance.currentLevel.spawnableScrap.Exists(x => x.spawnableItem.name == Assets.ItemNameList[Assets.ItemName.Comedy])) return true;
-            if (RoundManager.Instance.currentLevel.spawnableScrap.Exists(x => x.spawnableItem.name == Assets.ItemNameList[Assets.ItemName.Tragedy])) return true;
-            return false;
-        }
+        public override bool AddEventIfOnly() => Manager.SpawnExists(Assets.EnemyName.Masked);
 
-        public override void Execute()
-        {
-            // Remove masked scrap
-            int index = RoundManager.Instance.currentLevel.spawnableScrap.FindIndex(s => s.spawnableItem.name == Assets.ItemNameList[Assets.ItemName.Comedy]);
-            if (index != -1)
-            {
-                RoundManager.Instance.currentLevel.spawnableScrap.RemoveAt(index);
-                removedComedy = true;
-            }
-
-            index = RoundManager.Instance.currentLevel.spawnableScrap.FindIndex(s => s.spawnableItem.name == Assets.ItemNameList[Assets.ItemName.Tragedy]);
-            if (index != -1) 
-            { 
-                RoundManager.Instance.currentLevel.spawnableScrap.RemoveAt(index);
-                removedTragedy = true;
-            }
-
-            // Remove masked enemy
-            Manager.RemoveSpawn(Assets.EnemyName.Masked);
-        }
-
-        public override void OnShipLeave()
-        {
-            if(removedComedy)
-            {
-                RoundManager.Instance.currentLevel.spawnableScrap.Add(comedyReference);
-            }
-            if(removedTragedy)
-            {
-                RoundManager.Instance.currentLevel.spawnableScrap.Add(tragedyRefernece);
-            }
-
-            removedComedy = false; removedTragedy = false;
-        }
-
-        public override void OnGameStart()
-        {
-            removedComedy = false;
-            removedTragedy = false;
-        }
+        public override void Execute() => Manager.RemoveSpawn(Assets.EnemyName.Masked);
     }
 }
--- BrutalCompanyMinus/Plugin.cs
+++ BrutalCompanyMinus/Plugin.cs
@@ -24,7 +24,7 @@ namespace BrutalCompanyMinus
     {
         private const string GUID = "Drinkable.BrutalCompanyMinus";
         private const string NAME = "BrutalCompanyMinus";
-        private const string VERSION = "0.13.5";
+        private const string VERSION = "0.13.6";
         private static readonly Harmony harmony = new Harmony(GUID);
 
         void Awake()
